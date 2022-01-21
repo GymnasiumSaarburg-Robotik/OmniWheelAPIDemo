0c2928febabbcbd513224c85fe371d3ee16d8b6f
Joshua Benning <jabenning13@gmail.com>
2022-01-21 17:58:39 +0100

    Kommmmmmmentare

---
+++ readme.md
@@ -0,0 +1,6 @@
+Mehrere Möglichkeiten der Ausführung dieser DEMO:
+Der EV3Runner dient hierbei der Demonstration der tatsächlichen Bewegungen während
+der LocalRunner sich auf Berechnungen und Visualisierung der Vorgänge fokussiert,
+können beide Runner nicht im jeweils anderen Kontext verwendet werden.
+
+Der LocalRunner verwendet JFrames zur Darstellung der Matrix. 
--- src/de/lejostest/demo/LocalRunner.java
+++ src/de/lejostest/demo/LocalRunner.java
@@ -5,11 +5,23 @@ import de.lejostest.api.Vector2d;
 public class LocalRunner {
     public static void main(String[] args) {
 
+        /*
+        Idee später:
+        Ein-Dimensionaler Vektor spiegelt Geschwindigkeit wider.
+        Dieser Vektor wird dann entsprechend den Innenwinkeln der Basis gedreht.
+        Die drei resultierenden Vektoren sollten normiert sein in Richtung RU/s oder LE/s
+        Zusammensetzung zu Gesamtbewegung durch Addieren der Vektoren
+        TODO: Zusammensetzung überprüfen
+        TODO: Simples Test-GUI; Drei einstellbare Vektoren; Darstellung der finalen Fahrbahn
+        TODO: Überlegungen bezüglich Ableitung der drei Vektoren machen (Fahrrichtung -> Motoranweisungen)
+         */
 
-        Vector2d vector2d = new Vector2d(4.0, 1.0);
-        Vector2d vector2d1 = new Vector2d(1.0, -1.0);
-        vector2d.add(vector2d1);
-        System.out.println(vector2d.toString());
+
+        Vector2d vector1 = new Vector2d(4.0, 1.0);
+        Vector2d vector2 = new Vector2d(1.0, -1.0);
+        Vector2d vector3 = new Vector2d(1.0, -1.0);
+
+        Vector2d finalVector = vector1.add(vector2).add(vector3);
 
 
 
